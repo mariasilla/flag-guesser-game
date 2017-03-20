a6aac59f7f15e5d96b3a2cb7c2c2ce3bb323fb64
Maria Silla <maria.sylla@gmail.com>
2017-03-19 21:52:15 -0400

    updated gameResult function

--- flags.js
+++ flags.js
@@ -11,7 +11,7 @@ $("document").ready(function() {
      flagsGame();
    }
   });
-
+  let modalbody = $("modal-body");
   let playButton = $("#playButton");
   let flagModal = $("#flagModal");
   let flagImg = $(".flag");
@@ -20,15 +20,15 @@ $("document").ready(function() {
   let radioBtnThreeName = $(".radioBtnThree");
   let radioBtnFourName = $(".radioBtnFour");
   let nextBtn = $(".next");
-  // let answerButton = $(".answer");
+  let answerButton = $(".answer");
   let answerButtonOne = $(".one");
   let answerButtonTwo = $(".two");
   let answerButtonThree = $(".three");
   let answerButtonFour = $(".four");
   let popover = $('[data-toggle="popover"]');
-  let winner = 0;
-  let looser = 0;
-  // let score = 0;
+  // let winner = 0;
+  // let looser = 0;
+  let score = 0;
 
   // flagsArr.forEach(function(index){
       // console.log(flagsArr[0].Results.i.Name);
@@ -116,34 +116,40 @@ $("document").ready(function() {
       //FLAG MODALS START HERE
       function japanModal() {
         //change attributes of flagModal according to country flag
-          // flagImg.css("background-position", "-1px -8480px");
           flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/JP.png")
           radioBtnOneName.text(flagsArr[0].Results.JP.Name);
-          radioBtnTwoName.text(flagsArr[0].Results.FR.Name);
-          radioBtnThreeName.text(flagsArr[0].Results.BD.Name);
+          radioBtnTwoName.text(flagsArr[0].Results.UA.Name);
+          radioBtnThreeName.text(flagsArr[0].Results.JM.Name);
           radioBtnFourName.text(flagsArr[0].Results.US.Name);
-
+          $(".closeBtn").hide();
           answerButtonOne.click(function(){
-             winner+=1;
+             score+=1;
+            //  console.log("japan winner score is " + score);
              showRadioBtnOne();
             })
+
             answerButtonTwo.click(function(){
-              looser+=1;
+              score-=1;
               showRadioBtnTwo();
             })
+
             answerButtonThree.click(function(){
-                looser+=1;
+                 score-=1;
                 showRadioBtnThree();
-          })
-          answerButtonFour.click(function(){
-              looser+=1;
+            })
+
+           answerButtonFour.click(function(){
+               score-=1;
               showRadioBtnFour();
-         })
+            })
 
       }; // japanModal() ends here
 
       function canadaModal() {
+          answerButton.off();
+          $(".closeBtn").hide();
           showAllBtns();
+          answerButton.prop('checked', false);
           flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/CA.png");
           radioBtnOneName.text(flagsArr[0].Results.RU.Name);
           radioBtnTwoName.text(flagsArr[0].Results.JP.Name);
@@ -151,55 +157,64 @@ $("document").ready(function() {
           radioBtnFourName.text(flagsArr[0].Results.US.Name);
 
           answerButtonOne.click(function(){
-             looser+=1;
+              score-=1;
              showRadioBtnOne();
           })
-        answerButtonTwo.click(function(){
-               looser+=1;
+         answerButtonTwo.click(function(){
+               score-=1;
                showRadioBtnTwo();
          })
          answerButtonThree.click(function(){
-              winner+=1;
+              score+=1;
               showRadioBtnThree();
-              // popover.attr("data-content", "Correct!:) Click next!");
          })
          answerButtonFour.click(function(){
-                looser+=1;
+                score-=1;
                 showRadioBtnFour();
          })
 
       }; // canadaModal() ends here
 
       function usaModal(){
+          answerButton.off();
+          $(".closeBtn").hide();
           showAllBtns();
+          answerButton.prop('checked', false);
           flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/US.png");
-          radioBtnOneName.text(flagsArr[0].Results.RU.Name);
+          radioBtnOneName.text(flagsArr[0].Results.IT.Name);
           radioBtnTwoName.text(flagsArr[0].Results.US.Name);
           radioBtnThreeName.text(flagsArr[0].Results.CA.Name);
           radioBtnFourName.text(flagsArr[0].Results.JP.Name);
 
         answerButtonOne.click(function(){
-            looser+=1;
+           score-=1;
             showRadioBtnOne();
+
         })
        answerButtonTwo.click(function(){
              // popover.attr("data-content", "Correct!:) Click next!");
-             winner+=1;
+             score+=1;
              showRadioBtnTwo();
+
        })
        answerButtonThree.click(function(){
-            looser+=1;
-            showRadioBtnThree();
+          score-=1;
+          showRadioBtnThree();
+
        })
        answerButtonFour.click(function(){
-            looser+=1;
-            showRadioBtnFour();
+          score-=1;
+          showRadioBtnFour();
+
        })
 
       }; //usaModal() ends here
 
       function chinaModal(){
+          answerButton.off();
+          $(".closeBtn").hide();
           showAllBtns();
+          answerButton.prop('checked', false);
           flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/CN.png");
           radioBtnOneName.text(flagsArr[0].Results.GR.Name);
           radioBtnTwoName.text(flagsArr[0].Results.US.Name);
@@ -207,27 +222,33 @@ $("document").ready(function() {
           radioBtnFourName.text(flagsArr[0].Results.CL.Name);
 
         answerButtonOne.click(function(){
-           looser+=1;
+           score-=1;
            showRadioBtnOne();
+
         })
        answerButtonTwo.click(function(){
-             // popover.attr("data-content", "Correct!:) Click next!");
-             looser+=1;
+              score-=1;
              showRadioBtnTwo();
+
        })
        answerButtonThree.click(function(){
-            winner+=1;
-            showRadioBtnThree();
+          score+=1;
+          showRadioBtnThree();
+
        })
        answerButtonFour.click(function(){
-            looser+=1;
-            showRadioBtnFour();
+          score-=1;
+          showRadioBtnFour();
+
        })
 
      }; //ChinaModal() ends here
 
      function italyModal(){
+         answerButton.off();
+         $(".closeBtn").hide();
          showAllBtns();
+         answerButton.prop('checked', false);
          flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/IT.png");
          radioBtnOneName.text(flagsArr[0].Results.GR.Name);
          radioBtnTwoName.text(flagsArr[0].Results.US.Name);
@@ -235,27 +256,29 @@ $("document").ready(function() {
          radioBtnFourName.text(flagsArr[0].Results.IT.Name);
 
        answerButtonOne.click(function(){
-          looser+=1;
+          score-=1;
           showRadioBtnOne();
        })
       answerButtonTwo.click(function(){
-            // popover.attr("data-content", "Correct!:) Click next!");
-           looser+=1;
+           score-=1;
            showRadioBtnTwo();
       })
       answerButtonThree.click(function(){
-           looser+=1;
+           score-=1;
            showRadioBtnThree();
       })
       answerButtonFour.click(function(){
-           winner+=1;
+           score+=1;
            showRadioBtnFour();
       })
 
     }; //italyModal() ends here
 
     function russiaModal(){
+        answerButton.off();
+        $(".closeBtn").hide();
         showAllBtns();
+        answerButton.prop('checked', false);
         flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/RU.png");
         radioBtnOneName.text(flagsArr[0].Results.JP.Name);
         radioBtnTwoName.text(flagsArr[0].Results.RU.Name);
@@ -263,27 +286,31 @@ $("document").ready(function() {
         radioBtnFourName.text(flagsArr[0].Results.IT.Name);
 
       answerButtonOne.click(function(){
-         looser+=1;
+         score-=1;
          showRadioBtnOne();
       })
       answerButtonTwo.click(function(){
-           // popover.attr("data-content", "Correct!:) Click next!");
-          winner+=1;
+         score+=1;
           showRadioBtnTwo();
       })
       answerButtonThree.click(function(){
-          looser+=1;
+         score-=1;
+          // looser+=1;
+          // console.log("loose score is " + looser);
           showRadioBtnThree();
       })
       answerButtonFour.click(function(){
-          looser+=1;
-          showRadioBtnFour();
+         score-=1;
+         showRadioBtnFour();
       })
 
     }; //russiaModal() ends here
 
     function  chileModal(){
+        answerButton.off();
+        $(".closeBtn").hide();
         showAllBtns();
+        answerButton.prop('checked', false);
         flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/CL.png");
         radioBtnOneName.text(flagsArr[0].Results.CA.Name);
         radioBtnTwoName.text(flagsArr[0].Results.US.Name);
@@ -291,28 +318,30 @@ $("document").ready(function() {
         radioBtnFourName.text(flagsArr[0].Results.JM.Name);
 
       answerButtonOne.click(function(){
-         looser+=1;
+         score-=1;
          showRadioBtnOne();
       })
       answerButtonTwo.click(function(){
-           // popover.attr("data-content", "Correct!:) Click next!");
-          looser+=1;
-          showRadioBtnTwo();
+         score-=1;
+         showRadioBtnTwo();
       })
       answerButtonThree.click(function(){
-          winner+=1;
+         score+=1;
           showRadioBtnThree();
       })
       answerButtonFour.click(function(){
-          looser+=1;
-          showRadioBtnFour();
+         score-=1;
+         showRadioBtnFour();
       })
 
     }; //chileModal() ends here
 
 
     function jamaicaModal(){
+        answerButton.off();
+        $(".closeBtn").hide();
         showAllBtns();
+        answerButton.prop('checked', false);
         flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/JM.png");
         radioBtnOneName.text(flagsArr[0].Results.JM.Name);
         radioBtnTwoName.text(flagsArr[0].Results.CA.Name);
@@ -320,27 +349,29 @@ $("document").ready(function() {
         radioBtnFourName.text(flagsArr[0].Results.JP.Name);
 
       answerButtonOne.click(function(){
-         winner+=1;
+         score+=1;
          showRadioBtnOne();
       })
       answerButtonTwo.click(function(){
-           // popover.attr("data-content", "Correct!:) Click next!");
-          looser+=1;
-          showRadioBtnTwo();
+         score-=1;
+         showRadioBtnTwo();
       })
       answerButtonThree.click(function(){
-          looser+=1;
-          showRadioBtnThree();
+         score-=1;
+         showRadioBtnThree();
       })
       answerButtonFour.click(function(){
-          looser+=1;
-          showRadioBtnFour();
+         score-=1;
+         showRadioBtnFour();
       })
     }; //jamaicaModal() ends here
 
 
     function ukraineModal(){
+        answerButton.off();
+        $(".closeBtn").hide();
         showAllBtns();
+        answerButton.prop('checked', false);
         flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/UA.png");
         radioBtnOneName.text(flagsArr[0].Results.JM.Name);
         radioBtnTwoName.text(flagsArr[0].Results.JP.Name);
@@ -348,26 +379,28 @@ $("document").ready(function() {
         radioBtnFourName.text(flagsArr[0].Results.CA.Name);
 
       answerButtonOne.click(function(){
-         looser+=1;
+         score-=1;
          showRadioBtnOne();
       })
       answerButtonTwo.click(function(){
-           // popover.attr("data-content", "Correct!:) Click next!");
-          looser+=1;
+          score-=1;
           showRadioBtnTwo();
       })
       answerButtonThree.click(function(){
-          winner+=1;
+          score+=1;
           showRadioBtnThree();
       })
       answerButtonFour.click(function(){
-          looser+=1;
+          score-=1;
           showRadioBtnFour();
       })
     }; //ukraineModal() ends here
 
     function ethiopiaModal(){
+        answerButton.off();
+        $(".closeBtn").hide();
         showAllBtns();
+        answerButton.prop('checked', false);
         flagImg.attr("src", "http://www.geognos.com/api/en/countries/flag/ET.png");
         radioBtnOneName.text(flagsArr[0].Results.UA.Name);
         radioBtnTwoName.text(flagsArr[0].Results.IT.Name);
@@ -375,21 +408,20 @@ $("document").ready(function() {
         radioBtnFourName.text(flagsArr[0].Results.ET.Name);
 
       answerButtonOne.click(function(){
-         looser+=1;
+         score-=1;
          showRadioBtnOne();
       })
       answerButtonTwo.click(function(){
-           // popover.attr("data-content", "Correct!:) Click next!");
-          looser+=1;
+          score-=1;
           showRadioBtnTwo();
       })
       answerButtonThree.click(function(){
-          looser+=1;
+          score-=1;
           showRadioBtnThree();
       })
       answerButtonFour.click(function(){
-          winner+=1;
-          showRadioBtnFour();
+           score+=1;
+           showRadioBtnFour();
       })
     }; //ethiopiaModal() ends here
 
@@ -408,21 +440,15 @@ $("document").ready(function() {
          let counter = 1;
          nextBtn.on("click", function (event) {
            if(counter === 1) {
-           event.preventDefault();
-           countDownTimer();
            flagModal.modal('show');
            canadaModal();
            counter = 2;
            // nextBtn.removeClass("next").addClass("nextOne");
            } else if (counter === 2){
-             event.preventDefault();
-             countDownTimer();
              flagModal.modal('show');
              usaModal();
              counter = 3;
            } else if (counter === 3) {
-             event.preventDefault();
-             countDownTimer();
              flagModal.modal('show');
              chinaModal();
              counter = 4;
@@ -452,15 +478,15 @@ $("document").ready(function() {
 
          // functions to display winner or looser message
          function showWinnerModal() {
-          //  flagModal.modal('show');
-          //  if(winner === 2) {
            $("#winnerOrLooserModal").modal("show");
            $(".winner").text("You won!!! Congrats!!!");
            $(".winnerMessage").text("You can now check out these awesome TRAVEL DEALS!")
            $(".winnerGif").attr("src", "https://media.giphy.com/media/1eAG5O47bHjpK/giphy.gif");
            flagModal.modal('hide');
-
-          //  }
+           $(".reloadBtn").text("Play again");
+           $(".reloadBtn").click(function(){
+              location.reload();
+           })
          }; //showWinner() ends here
 
          function showLooserModal() {
@@ -468,33 +494,39 @@ $("document").ready(function() {
              $(".looser").text("Sorry! Try again!!!");
              $(".looserGif").attr("src", "https://media.giphy.com/media/auxWjSpDJ8544/giphy.gif");
              flagModal.modal('hide');
+             $("#skyWidget").hide();
+             $(".reloadBtn").text("Play again");
+             $(".reloadBtn").click(function(){
+                location.reload();
+             })
+
          }; //showLooser() ends here
 
          //function check if winner or looser
          function gameResult() {
-           if(winner > 5) {
+           if(score > 5) {
            showWinnerModal();
-           console.log("winner score is " + winner);
-         } else if (looser > 5){
+           console.log("winner score is " + score);
+         } else if (score < 5){
            showLooserModal();
-           console.log("looser score is " + looser);
+           console.log("looser score is " + score);
            }
          }
 
          //15 seconds timer
          //followed this tutorial http://www.smarttutorials.net/javascript-countdown-timer/
              let timer = $("#timer");
-             let countDownTime = 15;
+             let countDownTime = 50;
              let timeout;
              let x;
              function countDownTimer() {
              countDownTime-=1;
              timer.text(countDownTime);
              x = setTimeout(function(){ countDownTimer() }, 1000);
-               if (countDownTime < 0) {
-                 timer.text("Time out");
+               if (countDownTime === 0) {
                  stopIntervalFunc();
-                //  gameResult()
+                 gameResult();
+                //  $(".looser").text("TIME'S UP! Try again!!!");
                }
              }
 
